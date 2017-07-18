61f7e4e9fbe5ba075782c3b422b507eaf330ae2a
Jordan Cooper <jordan@cooperplanet.com>
2017-07-18 17:14:38 -0600

    Added content for the remainder of the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,11 +7,33 @@ export default class Home extends Component {
   render () {
     return (
       <div>
-        <div className="slideshow-container"></div>
-        <p className="intro-text">Create custom postcards from your mobile device</p>
-        <div className="separator"></div>
-        <h1 className="get-started-text">GET STARTED</h1>
-        <img src={arrow} alt="down arrow" className="down-arrow" />
+        <section>
+          <div className="slideshow-container"></div>
+          <p className="intro-text">Create custom postcards from your mobile device</p>
+          <div className="separator"></div>
+          <h1 className="get-started-text">GET STARTED</h1>
+          <img src={arrow} alt="down arrow" className="down-arrow" />
+        </section>
+        <section>
+          <div className="upload-container">
+            <div className="home-section-top">
+              <h1 className="home-section-title">Upload</h1>
+            </div>
+            <p className="home-section-description">Upload your photo, or choose one of ours</p>
+          </div>
+          <div className="customize-container">
+            <div className="home-section-top">
+              <h1 className="home-section-title">Customize</h1>
+            </div>
+            <p className="home-section-description">Pick your styles and add a message</p>
+          </div>
+          <div className="send-container">
+            <div className="home-section-top">
+              <h1 className="home-section-title">Send</h1>
+            </div>
+            <p className="home-section-description">Send a printed postcard to your friends</p>
+          </div>
+        </section>
       </div>
     )
   }
