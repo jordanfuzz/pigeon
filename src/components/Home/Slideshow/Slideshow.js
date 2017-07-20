ad747fa5fa256195c5bd39d87b5fa0dd6cad2685
Jordan Cooper <jordan@cooperplanet.com>
2017-07-20 11:12:05 -0600

    Add working slideshow with three images

--- src/components/Home/Slideshow/Slideshow.js
+++ src/components/Home/Slideshow/Slideshow.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import lake from './lake.jpg'
+import zebras from './zebras.jpg'
+import mountain from './mountain.jpg'
 import './Slideshow.css'
 import previous from './left-arrow.svg'
 import next from './right-arrow.svg'
@@ -11,7 +13,10 @@ export default class Slideshow extends Component {
     this.state = {
       currentSlide: 0
     }
+    this.handleNext = this.handleNext.bind(this)
+    this.handlePrevious = this.handlePrevious.bind(this)
   }
+
 
   handleNext(event) {
     event.preventDefault()
@@ -44,11 +49,28 @@ export default class Slideshow extends Component {
   }
 
   render() {
+    let source = lake
+
+    switch(this.state.currentSlide) {
+      case 0:
+        source = lake
+        break
+      case 1:
+        source = mountain
+        break
+      case 2:
+        source = zebras
+        break
+      default:
+        source = lake
+        console.log("used default")
+    }
+
     return(
 
       <div className="slideshow-container">
         <img onClick={this.handlePrevious} className="slideshow-button-left" src={previous} alt="Previous"/>
-        <img className="slideshow-image" src={lake} alt="Lake"/>
+        <img className="slideshow-image" src={source} alt="Lake"/>
         <img onClick={this.handleNext} className="slideshow-button-right" src={next} alt="Next"/>
       </div>
 
