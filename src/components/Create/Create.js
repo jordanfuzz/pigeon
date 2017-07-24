50e1125efb7dd234139b9ee10759dea8b0df4bd8
Jordan Cooper <jordan@cooperplanet.com>
2017-07-24 17:53:30 -0600

    Refactor css and separate creation components

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './Create.css'
 import Layout from './Layout/Layout'
+import Upload from './Upload/Upload'
 
 export default class Create extends Component {
 
@@ -8,10 +9,30 @@ export default class Create extends Component {
   constructor() {
     super()
     this.state = {
-      selectedLayout: 0
+      selectedLayout: 0,
+      currentStepIndex: 0
     }
 
     this.updateLayout = this.updateLayout.bind(this)
+    this.handleProceed = this.handleProceed.bind(this)
+    this.handleGoBack = this.handleGoBack.bind(this)
+    this.getCurrentStep = this.getCurrentStep.bind(this)
+  }
+
+  handleProceed(event) {
+    event.preventDefault()
+
+    this.setState({
+      currentStepIndex: this.state.currentStepIndex + 1
+    })
+  }
+
+  handleGoBack(event) {
+    event.preventDefault()
+
+    this.setState({
+      currentStepIndex: this.state.currentStepIndex - 1
+    })
   }
 
   updateLayout(layout) {
@@ -21,11 +42,29 @@ export default class Create extends Component {
     return layout
   }
 
+  getCurrentStep(index) {
+    switch(index) {
+      case 0:
+        return <Layout updateLayout={this.updateLayout} selectedLayout={this.state.selectedLayout} />
+      case 1:
+        return <Upload selectedLayout={this.state.selectedLayout} />
+      default:
+        return null
+    }
+  }
+
   render () {
+    let currentStep = this.getCurrentStep(this.state.currentStepIndex)
+
+
     return (
       <div className="create-main">
-        <Layout updateLayout={this.updateLayout} selectedLayout={this.state.selectedLayout} />
-        <div className="next-button">Next</div>
+        {currentStep}
+        <div className="button-container">
+          <div onClick={this.handleGoBack} className={`step-button previous ${this.state.currentStepIndex > 0 ? `shown-button` : `hidden-button`}`}>Previous</div>
+          <div onClick={this.handleProceed} className="step-button next">Next</div>
+        </div>
+
       </div>
     )
   }
--- src/components/Create/Upload/Upload.js
+++ src/components/Create/Upload/Upload.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react'
+import './Upload.css'
 
 export default class Upload extends Component {
 
+  countImages(layout) {
+    if(layout === 0)
+      return 1
+    else if(layout === 1)
+      return 2
+    else if(layout === 2 || layout === 3)
+      return 3
+    else return 4
+  }
+
   render() {
+    let imageCount
+    if(this.props) {
+      imageCount = this.countImages(this.props.selectedLayout)
+    }
+    let uploadSections = []
+    for (let i = 0; i < imageCount; i++) {
+      uploadSections.push(<div key={i}>
+        <h1 className="image-number">{`${i+1}  -`}</h1>
+        <div className="upload-section-button">Upload</div>
+        <p className="or-separator">-or-</p>
+        <div className="upload-section-button">Choose</div>
+        <br/>
+        <input className="public-checkbox" type="checkbox"/>
+        <p className="public-text">Make this image public?</p>
+        </div>)
+    }
+
     return (
-      <div>
-      </div>
-
-
-    )
+      <div className="upload-main">
+        <h1 className="title">Upload Photos</h1>
+        <div className={`selected-layout layout-${this.props.selectedLayout}`}></div>
+        <div className="upload-sections">
+          {uploadSections}
+        </div>
+      </div>)
 
   }
 
