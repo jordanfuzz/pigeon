4c705a8955e64b61462d0f3faee6d60f575b9fe8
Jordan Cooper <jordan@cooperplanet.com>
2017-07-30 07:25:30 -0600

    Display user input from recipient component on postcard back

--- src/components/Create/Recipient/Recipient.js
+++ src/components/Create/Recipient/Recipient.js
@@ -1,23 +1,52 @@
 import React, {Component} from 'react'
 import './Recipient.css'
 import PostcardBack from '../../PostcardBack/PostcardBack'
-import PropTypes from 'prop-types'
 
 class Recipient extends Component {
   constructor() {
     super()
 
     this.state = {
+      recipientName: "",
+      recipientAddress: "",
+      recipientCity: "",
+      recipientState: "",
+      recipientPostalCode: "",
+      recipientCountry: ""
 
     }
   }
 
+  handleChange(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
   render() {
+    let address = (<div>
+      <div>{this.state.recipientName}</div>
+      <div>{this.state.recipientAddress}</div>
+      <div>{this.state.recipientCity ? `${this.state.recipientCity} ${this.state.recipientState}, ${this.state.recipientPostalCode}` : ``}</div>
+      <div>{this.state.recipientCountry}</div>
+    </div>)
 
 
     return(<div className="recipient-main">
       <h1 className="title">Enter the Recipient</h1>
-      <PostcardBack />
+      <PostcardBack address={address} />
+      <h6 className="recipient-header">Name:</h6>
+      <input className="inline" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientName" value={this.state.recipientName} type="text"/>
+      <h6 className="recipient-header">Street Address:</h6>
+      <input className="inline" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientAddress" value={this.state.recipientAddress} type="text"/>
+      <h6 className="recipient-header">City:</h6>
+      <input className="inline" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientCity" value={this.state.recipientCity} type="text"/>
+      <h6 className="recipient-header">State:</h6>
+      <input className="inline state" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientState" value={this.state.recipientState} type="text"/>
+      <h6 className="recipient-header">Postal Code:</h6>
+      <input className="inline" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientPostalCode" value={this.state.recipientPostalCode} type="text"/>
+      <h6 className="recipient-header">Country:</h6>
+      <input className="inline" onChange={(e) => this.handleChange(e.target.name, e.target.value)} name="recipientCountry" value={this.state.recipientCountry} type="text"/>
     </div>)
   }
 
--- src/components/PostcardBack/PostcardBack.js
+++ src/components/PostcardBack/PostcardBack.js
@@ -7,11 +7,15 @@ class PostcardBack extends Component {
 
 
   render () {
+    let address
+    if(this.props)
+      address = this.props.address
     return (
       <div className="postcard-back-main">
         <div className="postcard">
           <div style={{"fontFamily":`${this.context.selectedFont}, sans-serif`}} className="postcard-text">{this.context.message}</div>
           <div className="divider"></div>
+          <div className="postcard-address">{address}</div>
           <div className="stamp"></div>
         </div>
       </div>)
